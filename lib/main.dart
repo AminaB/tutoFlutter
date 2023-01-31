84f8804c95da365c1522c6e5e4d9d3e314c4343b
AminaB <aminatou.barry97@gmail.com>
2023-01-31 16:14:08 +0100

    sttless and sttfull widget on button

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( Myapp());
+  runApp(Myapp());
 }
 
-class Myapp extends StatelessWidget{
+class Myapp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home : Text('Hello'),);
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyappState();
+  }
+}
+class MyappState extends State<Myapp> {
+
+  int questionIndex=0;
+  void answerQuestion(){
+    setState(() {
+      questionIndex=questionIndex+1;
+    }
+    );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    var quetions = ['Your color', 'your animal'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(children: [Text(quetions[questionIndex]),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer1'),),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer2'),),
+
+        ]
+        ), //
+      ),
+    );
+  }
 }
---
+++ note.md
@@ -0,0 +1,9 @@
+# Notes
+
+A body can take only one widget, but we can use column widget ( lists of widgets)
+
+## Button
+
+- in onpressed, place the name of the function (ex : action, not action())
+- we have to use sttefull widgets when the internal data change, sttless wdgts can't re-run build() when its properties change
+- detaStage(()); update the widget.
