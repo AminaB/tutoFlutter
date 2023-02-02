c4b81d1ebbfaa9f0aacb2f4990bc6773a54d8b87
AminaB <aminatou.barry97@gmail.com>
2023-02-02 15:59:10 +0100

    ListViews.builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,11 +10,11 @@ TransactionList(this.transactions);
 @override
   Widget build(BuildContext context) {
     return   Container(
-      height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children : transactions.map((e) =>
-              Card(
+      height: 500,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+              return Card(
                 elevation: 5,
                 child:Row(
                   children: [
@@ -22,7 +22,7 @@ TransactionList(this.transactions);
                       margin: EdgeInsets.symmetric(vertical: 10,horizontal: 15),
                       decoration: BoxDecoration(border:Border.all(color: Colors.purple,width: 2)),
                       padding: EdgeInsets.all(10),
-                      child: Text('£' + e.amount.toString(),
+                      child: Text('£' + transactions[index].amount.toString(),
                         style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.purple),
 
                       ),
@@ -30,17 +30,18 @@ TransactionList(this.transactions);
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(e.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize:15 ),),
-                        Text(DateFormat().format(e.date),style: TextStyle(color: Colors.grey,fontSize:10 ))
+                        Text(transactions[index].title,style: TextStyle(fontWeight: FontWeight.bold,fontSize:15 ),),
+                        Text(DateFormat().format(transactions[index].date),style: TextStyle(color: Colors.grey,fontSize:10 ))
                       ],
                     ),
                   ],
 
                 ),
-              )
-          ).toList(),
-        ),
-      ),
+              );
+            }
+        ,
+
+      )
     );
   }
 }
