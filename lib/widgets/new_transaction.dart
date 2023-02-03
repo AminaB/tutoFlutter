7668c5296d54c9b11213fe7f250dd8a1f1bdeace
AminaB <aminatou.barry97@gmail.com>
2023-02-03 11:03:18 +0100

    on submitted and fixed 2 decimal

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -4,6 +4,14 @@ import 'package:flutter/material.dart';
 
 class Newtransaction extends StatelessWidget {
   final Function addTx;
+  void submitData(){
+    if( titleController.text.isEmpty || double.parse(amountController.text)<0){
+      return;
+    }
+    addTx(
+      titleController.text, double.parse(amountController.text)
+    );
+  }
   final titleController= TextEditingController();
   final amountController= TextEditingController();
   Newtransaction(this.addTx);
@@ -19,17 +27,21 @@ class Newtransaction extends StatelessWidget {
               TextField(
                 decoration:InputDecoration(labelText: 'Title'),
                 controller: titleController,
+                onSubmitted: (_)=> submitData,
               ),
               TextField(
                 decoration: InputDecoration(labelText: 'amount'),
                 controller: amountController,
                 keyboardType:  TextInputType.numberWithOptions(decimal: true),
                 //onChanged: (value) => amount=value,
+                onSubmitted: (_)=> submitData,
               ),
               TextButton(
+
                 onPressed:()  { addTx(titleController.text,double.parse(amountController.text));
                   print(titleController.text);
                 },
+
                 child: Text('Add transaction'),
                 style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.purple)),
               )
