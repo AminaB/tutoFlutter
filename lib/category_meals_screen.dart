24ade1f5a9c082a07391db609d5dd8a29c796eed
AminaB <aminatou.barry97@gmail.com>
2023-02-21 15:18:35 +0100

    selecting meal for a chosen category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tuto_flutter/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static var routeName= '/category_meals';
@@ -10,17 +11,19 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs=ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle=routeArgs['title'];
-    //final categoryId=routeArgs['id'];
-
+    final categoryId=routeArgs['id'];
+    final categoryMeals=DUMMY_MEALS.where((element) => element.categories.contains(categoryId)).toList();
     return  Center(
       child: Scaffold(
         appBar: AppBar(
           title: Text(categoryTitle.toString()),
         ),
-          body: Center(
-              child: Text('The Recipes For The Category')
+          body: ListView.builder(itemBuilder: (ctx, index) =>
+              Text(categoryMeals[index].title),
+              itemCount: categoryMeals.length,
+          ),
+
           )
-      ),
     );
   }
 }
