156ad1d9e1315b88caaac36ea83c9fc7e21497be
AminaB <aminatou.barry97@gmail.com>
2023-03-08 15:45:38 +0100

    theming and styling list of products

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      /*  canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue).copyWith(secondary: Colors.orange),
         fontFamily: 'OpenSans',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          bodySmall: TextStyle(
-            color: Color.fromRGBO(20, 51, 51, 1),
-
-          ),
-            bodyMedium: const TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1)
-            ),
-            titleSmall: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'Quicksand',
-                fontWeight: FontWeight.bold
-            ),
-            titleMedium: const TextStyle(
-                fontSize: 25,
-                fontFamily: 'Quicksand',
-                fontWeight: FontWeight.bold
-            ),
-            titleLarge: const TextStyle(
-                fontSize: 30,
-                fontFamily: 'Quicksand',
-                fontWeight: FontWeight.bold
-            )
-        ), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),*/
-
       ),
-      /*initialRoute: '/',
-      routes: {
-        //'/':(ctx)=> TabsScreen(_favoritedMeals),
-       // CategoryMealsScreen.routeName: (ctx)=>CategoryMealsScreen(_availableMeals),
-        //MealDetailScreen.routeName:(ctx)=>MealDetailScreen(_toogleFavorite,_isMealFavorite),
-        //FilterScreen.routeName:(ctx)=>FilterScreen(_filters,_setFilters),
-      },
-      onGenerateRoute: (settings) {
-        // print(settings.arguments);
-        // if(settings.name=='/meal-details'){
-        //     return;
-        // }else if(settings.name=='/something-else'){
-        //   return;
-        // }else{
-        //   MaterialPageRoute(builder: (context) => CategoryMealsScreen(),);
-        // }
-      },*/
+      // /*  canvasColor: Color.fromRGBO(255, 254, 229, 1),
+      //   fontFamily: 'OpenSans',
+      //   textTheme: ThemeData.light().textTheme.copyWith(
+      //     bodySmall: TextStyle(
+      //       color: Color.fromRGBO(20, 51, 51, 1),
+      //
+      //     ),
+      //       bodyMedium: const TextStyle(
+      //           color: Color.fromRGBO(20, 51, 51, 1)
+      //       ),
+      //       titleSmall: const TextStyle(
+      //           fontSize: 20,
+      //           fontFamily: 'Quicksand',
+      //           fontWeight: FontWeight.bold
+      //       ),
+      //       titleMedium: const TextStyle(
+      //           fontSize: 25,
+      //           fontFamily: 'Quicksand',
+      //           fontWeight: FontWeight.bold
+      //       ),
+      //       titleLarge: const TextStyle(
+      //           fontSize: 30,
+      //           fontFamily: 'Quicksand',
+      //           fontWeight: FontWeight.bold
+      //       )
+      //   ), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),*//*
+      //
+      // ),
+      // *//*initialRoute: '/',
+      // routes: {
+      //   //'/':(ctx)=> TabsScreen(_favoritedMeals),
+      //  // CategoryMealsScreen.routeName: (ctx)=>CategoryMealsScreen(_availableMeals),
+      //   //MealDetailScreen.routeName:(ctx)=>MealDetailScreen(_toogleFavorite,_isMealFavorite),
+      //   //FilterScreen.routeName:(ctx)=>FilterScreen(_filters,_setFilters),
+      // },
+      // onGenerateRoute: (settings) {
+      //   // print(settings.arguments);
+      //   // if(settings.name=='/meal-details'){
+      //   //     return;
+      //   // }else if(settings.name=='/something-else'){
+      //   //   return;
+      //   // }else{
+      //   //   MaterialPageRoute(builder: (context) => CategoryMealsScreen(),);
+      //   // }
+      // },*/
       home: ProductsOverviewScreen(),
     );
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -11,13 +11,24 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black45,
-          leading: IconButton(icon: const Icon(Icons.favorite), onPressed: () {  },),
-          title: Text(title, textAlign: TextAlign.center,),
-          trailing: IconButton(icon:const Icon(Icons.shopping_cart), onPressed: () {  },),
+        footer: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: const Icon(Icons.favorite),
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {  },),
+            title: Text(title, textAlign: TextAlign.center ,),
+            trailing: IconButton(
+              icon:const Icon(
+                Icons.shopping_cart,
+              ),
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {  },),
 
       ),
+        ),
         child: Image.network(
             imageUrl, fit: BoxFit.fill
         ),
