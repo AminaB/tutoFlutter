ecdd0631bdcf14ee8d76d4faa2bdee1b49e650bf
AminaB <aminatou.barry97@gmail.com>
2023-03-15 15:05:46 +0100

    using consumer widget to reload favorite button only when there is a change

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:tuto_flutter/screens/product_detail_screen.dart';
 
 import '../providers/product.dart';
-import '../providers/products.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -16,35 +15,47 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product=Provider.of<Product>(context);
-    return GridTile(
-        footer: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
-              icon:  Icon(
-                  product.isFavorite ? Icons.favorite: Icons.favorite_border
+    return   ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: GridTile(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed(
+                    ProductDetailScreen.routeName,
+                    arguments: product.id,
+                  );
+                },
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () { product.toggleFavoriteStatus() ;},),
-            title: Text(product.title, textAlign: TextAlign.center ,),
-            trailing: IconButton(
-              icon:const Icon(
-                Icons.shopping_cart,
+              footer: GridTileBar(
+                backgroundColor: Colors.black87,
+                leading: Consumer<Product>(
+                  builder: (ctx, product, child) => IconButton(
+                    icon: Icon(
+                      product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                    ),
+                    color: Theme.of(context).colorScheme.secondary,
+                    onPressed: () {
+                      product.toggleFavoriteStatus();
+                    },
+                  ),
+                ),
+                title: Text(
+                  product.title,
+                  textAlign: TextAlign.center,
+                ),
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.shopping_cart,
+                  ),
+                  onPressed: () {},
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
               ),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () {  },),
-
-      ),
-        ),
-        child: GestureDetector(
-          child: Image.network(
-              product.imageUrl, fit: BoxFit.fill
-          ),
-          onTap: (){
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: product.id);
-          } ,
-        ),
-    );
+            ),
+          );
   }
 }
