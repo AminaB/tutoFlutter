888267f62d8c778ec9ea3b3d2ce0076fb7ea15c2
AminaB <aminatou.barry97@gmail.com>
2023-03-21 14:22:02 +0100

    fill product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,6 +18,28 @@ class ProductDetailScreen extends StatelessWidget {
     ).findById(productId);
     return Scaffold(
       appBar: AppBar(title: Text(loadedProduct.title),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,width: double.infinity,),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('\$${loadedProduct.price}', style: TextStyle(color: Colors.grey, fontSize: 20),),
+            SizedBox(
+              height:10 ,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(loadedProduct.description, textAlign: TextAlign.center,),
+            )
+        ]
+        ),
+      ),
+      
     );
   }
 }
