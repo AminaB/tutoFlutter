eb181937e062a3b219af194a32c3ea1636c0c089
AminaB <aminatou.barry97@gmail.com>
2023-03-23 14:32:57 +0100

    adding focus listener to imageUrlFocuseNode for not losing focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,14 +10,32 @@ class EditProductsScreen extends StatefulWidget {
 class _EditProductsScreenState extends State<EditProductsScreen> {
   final _priceFocusNode=FocusNode();
   final _descriptionFocusNode=FocusNode();
+  final _imageUrlController=TextEditingController();
+  final _imageUrlFocusNode=FocusNode();
 
   @override
+  void initState(){
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+
+
+  }
+  @override
   void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
     super.dispose();
   }
+  void _updateImageUrl(){
+    if(!_imageUrlFocusNode.hasFocus){
+      setState(() {
 
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -50,6 +68,32 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
 
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top: 8,right: 10),
+                    decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter URL')
+                        : FittedBox(
+                          child: Image.network(_imageUrlController.text),
+                          fit: BoxFit.cover,
+                        ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration:  InputDecoration(labelText: 'Image URL',),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  )
+                ],
               )
             ],
           ),
