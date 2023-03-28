21ba25c50afaa4316ce38cbca4e6913faa696bdc
AminaB <aminatou.barry97@gmail.com>
2023-03-28 14:20:00 +0200

    transform data fetch from db to products list and display them to shop

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -19,6 +19,7 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _isInit=true;
+  var _isLoading=false;
   @override
   void initState() {
     //Provider.of<Products>(context); do not do that i initstate
@@ -28,7 +29,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   void didChangeDependencies() {
     if(_isInit){
-      Provider.of<Products>(context).fetchAndSetProducts();
+      setState(() {
+        _isLoading=true;
+
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_){
+        setState(() {
+          _isLoading=false;
+
+        });
+      });
     }
     _isInit=false;
     super.didChangeDependencies();
@@ -74,7 +84,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading
+          ? const Center(
+            child: CircularProgressIndicator(),
+          )
+          :ProductsGrid(_showOnlyFavorites),
     );
   }
 }
