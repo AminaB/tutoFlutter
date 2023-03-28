283e5ae6b6060ab09e0b938511c4bb36983e6ca6
AminaB <aminatou.barry97@gmail.com>
2023-03-28 14:42:59 +0200

    pull to refresh user products

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -8,6 +8,9 @@ import 'package:tuto_flutter/widgets/user_product_item.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName='/user-products';
+  Future<void> _refreshProducts(BuildContext context) async{
+    await Provider.of<Products>(context,listen: false).fetchAndSetProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +27,18 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-            itemBuilder: (_, i) => Column(children: [
-              UserProductItem(productsData.items[i].title, productsData.items[i].imageUrl,productsData.items[i].id),
-              const Divider(),
-            ]
-            ),
-            itemCount: productsData.items.length,
+      body: RefreshIndicator(
+        onRefresh: ()=>_refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+              itemBuilder: (_, i) => Column(children: [
+                UserProductItem(productsData.items[i].title, productsData.items[i].imageUrl,productsData.items[i].id),
+                const Divider(),
+              ]
+              ),
+              itemCount: productsData.items.length,
+          ),
         ),
       ),
 
