c1c395bf56f9baf9c41c134ce82b39420ebf05f2
AminaB <aminatou.barry97@gmail.com>
2023-03-30 14:58:21 +0200

    login user

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,8 +7,8 @@ class Auth with ChangeNotifier{
   late String _token;
   late DateTime _expiryDate;
   late String _userId;
-  Future<void> signup(String email, String password) async{
-    final url =Uri.parse("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= AIzaSyAk68qMoW3ta7N5XecQPlfxu21-t6AesqA");
+  Future<void> _authenticate(String email, String password, String urlSegment) async{
+    final url =Uri.parse("https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key= AIzaSyAk68qMoW3ta7N5XecQPlfxu21-t6AesqA");
     //    final url=Uri.parse("https://flutter-update-14e05-default-rtdb.europe-west1.firebasedatabase.app/products.json");
     final response=await http.post(url,body: json.encode({
       'email':email,
@@ -16,5 +16,16 @@ class Auth with ChangeNotifier{
       'returnSecureToken':true
     }));
     print(json.decode(response.body));
+  }
+  Future<void> signup(String email, String password) async{
+   // final url =Uri.parse("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= AIzaSyAk68qMoW3ta7N5XecQPlfxu21-t6AesqA");
+    //    final url=Uri.parse("https://flutter-update-14e05-default-rtdb.europe-west1.firebasedatabase.app/products.json");
+    return _authenticate(email, password, "signUp");
+
+  }
+  Future<void> login(String email, String password) async{
+    final url =Uri.parse("hhttps://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key= AIzaSyAk68qMoW3ta7N5XecQPlfxu21-t6AesqA");
+    //    final url=Uri.parse("https://flutter-update-14e05-default-rtdb.europe-west1.firebasedatabase.app/products.json");
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
