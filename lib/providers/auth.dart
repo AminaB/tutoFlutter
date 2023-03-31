381f803cdadaba9aac0bf2bafb7326660427c0c4
AminaB <aminatou.barry97@gmail.com>
2023-03-31 10:58:20 +0200

    manage signup and login errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:tuto_flutter/models/http_exception.dart';
 
 class Auth with ChangeNotifier{
   late String _token;
@@ -9,13 +10,21 @@ class Auth with ChangeNotifier{
   late String _userId;
   Future<void> _authenticate(String email, String password, String urlSegment) async{
     final url =Uri.parse("https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key= AIzaSyAk68qMoW3ta7N5XecQPlfxu21-t6AesqA");
+    try{
+      final response=await http.post(url,body: json.encode({
+        'email':email,
+        'password':password,
+        'returnSecureToken':true
+      }));
+      final responseData=json.decode(response.body);
+      if(responseData['error']!=null){
+        throw HttpException(responseData['error']['message']);
+      }
+    }catch(error){
+      rethrow;
+    }
     //    final url=Uri.parse("https://flutter-update-14e05-default-rtdb.europe-west1.firebasedatabase.app/products.json");
-    final response=await http.post(url,body: json.encode({
-      'email':email,
-      'password':password,
-      'returnSecureToken':true
-    }));
-    print(json.decode(response.body));
+
   }
   Future<void> signup(String email, String password) async{
    // final url =Uri.parse("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= AIzaSyAk68qMoW3ta7N5XecQPlfxu21-t6AesqA");
